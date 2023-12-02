b8e62e68389c84dbfa9d95d0dfad08fc2ae0c2f8
Sergii <seregaonma@gmail.com>
2023-12-02 19:13:02 +0200

    add required functions for block

--- js/main.js
+++ js/main.js
@@ -2,11 +2,55 @@ const blockRef = document.querySelector('.block');
 const bodyRef = document.querySelector('.parent');
 const messageRef = document.querySelector('.message');
 
-let blockInitialX = (bodyRef.clientWidth - blockRef.clientWidth) / 2;
-let blockInitialY = (bodyRef.clientHeight - blockRef.clientHeight) / 2;
+const blockInitialX = (bodyRef.clientWidth - blockRef.clientWidth) / 2;
+const blockInitialY = (bodyRef.clientHeight - blockRef.clientHeight) / 2;
+const blockInitialHeight = blockRef.clientHeight;
+const blockInitialWidth = blockRef.clientWidth;
 
 let blockCurrentY = blockInitialY;
 let blockCurrentX = blockInitialX;
 
 blockRef.style.left = blockCurrentX + 'px';
 blockRef.style.top = blockCurrentY + 'px';
+
+const bodyRightBorder = bodyRef.clientWidth - cubeRef.clientWidth;
+const bodyBottomBorder = bodyRef.clientHeight - cubeRef.clientHeight;
+
+const showMessage = () => {
+  if(blockRef.style.top < bodyRef.clientHeight + 'px' || blockRef.style.top > bodyRef.clientTop + 'px' ||  blockRef.style.left < bodyRef.clientWidth + 'px' ||  blockRef.style.left > bodyRef.clientLeft + 'px') {
+    messageRef.classList.remove('message');
+    setTimeout(hideMessage, 2000);
+  }
+}
+
+const hideMessage = () => {
+  messageRef.classList.add('message');
+}
+
+const blockOriginalSize = () => {
+  blockRef.style.height = blockInitialHeight + 'px';
+  blockRef.style.width = blockInitialWidth + 'px';
+  setInterval(() => blockRef.style.transition = 0 + 's', 1000);
+}
+
+const blockSit  = () => {
+  const newHeight = blockInitialHeight * 0.4;
+  const newWidth = blockInitialWidth * (1 + 0.25);
+  blockRef.style.height = newHeight + 'px';
+  blockRef.style.width = newWidth + 'px';
+  blockRef.style.transition = 0.3 + 's';
+  document.addEventListener('keyup', blockOriginalSize);
+}
+
+const blockBeforeJumping = () => {
+  blockRef.style.top = blockCurrentY + 'px';
+}
+
+const blockJump = () => {
+  const jumpY = blockCurrentY - 10;
+  blockRef.style.top = jumpY + 'px';
+  if(blockRef.style.top <= bodyRef.clientTop + 'px') {
+    showMessage();
+  }
+  document.addEventListener('keyup', blockBeforeJumping);
+}
